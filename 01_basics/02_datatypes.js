d13611d45bb30112a373fddd7713acba4a000a95
Ziegler_Natta <153724823+SudarshanDwivedi@users.noreply.github.com>
2025-09-05 13:02:31 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS Code as a newer version
+
+//alert("hello")
+// alert(3+3)  we are using node js not the browser
+
+let name= "suddi"
+let age=21
+let isLoggedIn= false
+
+
+// number => 2 to the power 53
+// bigint
+// string=> ""
+// boolean=> true/false
+// null => standalone value
+// undefined
+// symbol => used for unique
+
+
+// object
+
+console.log(typeof "sudarshan")
+console.log(typeof null)  // type is object
+console.log(typeof undefined)
+
