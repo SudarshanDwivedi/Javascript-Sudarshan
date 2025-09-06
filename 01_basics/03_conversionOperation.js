c836847f55e54b9a23273c650ce35fc1ba74aab4
Ziegler_Natta <153724823+SudarshanDwivedi@users.noreply.github.com>
2025-09-06 12:58:56 +0000

    comparison operators

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -26,3 +26,35 @@ let someNumber= 33
 let stringNum =  String(someNumber)
 console.log(someNumber)
 console.log(typeof stringNum)
+
+
+
+//***************operations ***************
+
+
+let value = 3
+let negval= -value
+console.log(negval)
+
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2/2)
+console.log(2**2)
+console.log(2%2)
+
+let str1= "hello"
+let str2= " suddi"
+let str3= str1+ str2
+console.log(str3)
+
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + 2 +2)
+console.log(1 + 2 + "2")
+
+
+let gameCounter = 100
+gameCounter++
+
+console.log(gameCounter)
---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,14 @@
+//one can easily do the simple comparisions and the output will be in boolean
+// some other kind of comparisons are as follows
+
+console.log(null > 0)
+console.log(null == 0)
+console.log(null < 0)
+console.log(null != 0)
+console.log(null <= 0)
+console.log(null >= 0)
+
+console.log("2"=== 2) //it checks the data strictly and also checks the datatype and if not same then it will give the false
+console.log("2"==2) // this will give true because the data type is not checked
+
+ 
