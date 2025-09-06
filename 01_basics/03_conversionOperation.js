b8399c0d20882e65b767327a64a5839d2a177a03
Ziegler_Natta <153724823+SudarshanDwivedi@users.noreply.github.com>
2025-09-06 12:16:26 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = "33"
+let score2= "23anx"
+
+console.log(typeof score2)
+
+console.log(typeof score)
+
+let valInNumber = Number(score)
+let valInNumber2 = Number(score2)
+
+console.log(typeof valInNumber)
+console.log(typeof valInNumber2)
+console.log(valInNumber2)
+
+
+let isLoggedIn = 1;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1 => true
+// "" => false
+// "suddi" => true
+
+let someNumber= 33
+let stringNum =  String(someNumber)
+console.log(someNumber)
+console.log(typeof stringNum)
