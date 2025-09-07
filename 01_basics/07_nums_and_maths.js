f0e69ef682884db41b1d56bbc58d1c9924c63936
Ziegler_Natta <153724823+SudarshanDwivedi@users.noreply.github.com>
2025-09-07 20:10:16 +0000

    learned nums

---
+++ 01_basics/07_nums_and_maths.js
@@ -0,0 +1,18 @@
+const score = 500
+console.log(score);
+
+const balance= new Number(500)
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const otherno = 23.4344
+console.log(otherno.toPrecision(2))
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString('en-IN'));// this will give comma separated numbers
+
+
+// ++++++++++++++++++  maths  ++++++++++++++++++
+
