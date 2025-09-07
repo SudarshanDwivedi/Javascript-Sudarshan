e084d2ee0fc06823fa49d3d3254797b34710f802
Ziegler_Natta <153724823+SudarshanDwivedi@users.noreply.github.com>
2025-09-07 04:00:01 +0000

    stack and heap memory

--- 01_basics/05_datatypes_summary.js
+++ 01_basics/05_datatypes_summary.js
@@ -33,3 +33,41 @@ console.log(typeof myObj)
 //Boolean -> boolean
 // Null -> Object
 // function-> function
+
+
+
+
+
+
+
+
+
+
+//********memory************************
+
+// stack memory(primitive)   heap memory(non primitive)
+
+let myname= "sudarshan dwiwedi"
+
+let anothername= myname
+anothername= "priya"
+
+console.log(anothername)
+console.log(myname)
+
+// in stack memory the copy of the varibale is given and changes made are done to it are not done to the original
+
+let user1= {
+    email: "suddi@gmail.com",
+    upi: "8532825920@sbi"
+}
+
+// we can see that this object is non primitive datatype and it will have the heap memory and in heap there is no copy made rather the reference of the memory goes and any hanges made to the value will reflect in the original one
+let user2 = user1
+
+console.log(user2)
+
+user2.email= "priya@gmail.com" // here we are changing the emaol value of user 2 and if we print the value of user 1 then the changed value will be seen
+
+console.log(user1.email)
+console.log(user1)
